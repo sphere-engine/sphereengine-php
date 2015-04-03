fd907be40908c3917b0dd70a3ed801b066fb377c
Robert Lewoń <lewon.robert@gmail.com>
2015-04-03 18:08:19 +0200

    Commit ze strukturą biblioteki

---
+++ src/SphereEngineAPI.php
@@ -0,0 +1,25 @@
+
+/**
+ * Sphere Engine API
+ *
+ * LICENSE
+ *
+ *
+ * Tu treść licencji
+ *
+ *
+ * @copyright  Copyright (c) 2015 Sphere Research Labs (http://sphere-research.com)
+ * @license    link do licencji
+ * @version    0.1
+ */
+
+
+/**
+ * Opis klasy
+ *
+ */
+class SphereEngineAPI {
+
+	
+
+}
